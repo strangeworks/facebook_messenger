49ce4ddb925976afc8f3564dd73fa61b3a676228
Iurii Plugatariov <plugatariov@gmail.com>
2017-07-16 15:34:51 +0300

    support environment variables config

--- config/config.exs
+++ config/config.exs
@@ -8,16 +8,16 @@ config :logger,
 case Mix.env do
   :test ->
     config :facebook_messenger,
-      facebook_page_token: "PAGE_TOKEN",
-      challenge_verification_token: "VERIFY_TOKEN",
+      facebook_page_token: System.get_env("PAGE_TOKEN") || "PAGE_TOKEN",
+      challenge_verification_token: System.get_env("VERIFY_TOKEN") || "VERIFY_TOKEN",
       endpoint: "/messenger/webhook",
       request_manager: FacebookMessenger.RequestManager.Mock,
       responder: FacebookMessenger.Responder.Mock
 
   :dev ->
     config :facebook_messenger,
-      facebook_page_token: "PAGE_TOKEN",
-      challenge_verification_token: "VERIFY_TOKEN",
+      facebook_page_token: System.get_env("PAGE_TOKEN") || "PAGE_TOKEN",
+      challenge_verification_token: System.get_env("VERIFY_TOKEN") || "VERIFY_TOKEN",
       endpoint: "/messenger/webhook",
       request_manager: FacebookMessenger.RequestManager,
       responder: FacebookMessenger.Responder
